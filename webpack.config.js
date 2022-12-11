94cbce399799a201fa3746838867417b1f54b1df
gobackk1 <k.kwbt4@icloud.com>
2022-12-11 16:42:00 +0900

    minify js

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,18 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
+const NODE_ENV = process.env.NODE_ENV
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isProduction = process.env.NODE_ENV === 'production'
 module.exports = {
-  mode: 'development',
   entry: './src/main.ts',
+  // context: path.resolve(__dirname, './src'),
   output: {
     path: `${__dirname}/dist`,
     filename: 'main.js'
   },
-  devtool: 'source-map',
   module: {
     rules: [
       {
@@ -75,18 +78,40 @@ module.exports = {
     }
   },
   target: ['web', 'es5'],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-    port: 8081,
-    open: true
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
     }),
     new VueLoaderPlugin(),
     new VuetifyLoaderPlugin()
-  ]
+  ],
+  ...(isProduction
+    ? {
+        optimization: {
+          minimize: true,
+          minimizer: [[new TerserPlugin({ terserOptions: { compress: { drop_console: true } } })]]
+        }
+      }
+    : {
+        devtool: isDevelopment ? 'source-map' : false,
+        devServer: {
+          static: {
+            directory: path.join(__dirname, 'public')
+          },
+          port: 8081,
+          open: true
+        }
+      })
 }
