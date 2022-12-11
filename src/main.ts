d46fe261446009d87896cbc2cee98b6ad41ac02e
gobackk1 <k.kwbt4@icloud.com>
2022-12-11 18:28:18 +0900

    install vue router

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import vuetify from '@/plugins/vuetify'
+import router from '@/plugins/router'
 import MyButton from '@/components/atoms/button.vue'
 
 Vue.config.productionTip = false
 new Vue({
   el: '#app',
   vuetify,
+  router,
   components: { MyButton },
   template: `<v-app>
-      <h1>Hello Vue.js! </h1>
+      <router-link to="/">to home</router-link>
+      <router-link to="/about">to about</router-link>
+      <router-view />
       <MyButton text="update" />
    </v-app>`
 })
