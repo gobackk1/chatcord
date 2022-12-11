861770ce6652e182df974b2f98a2d9f8cf5bd2bc
gobackk1 <k.kwbt4@icloud.com>
2022-12-11 18:37:58 +0900

    install vuex

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import vuetify from '@/plugins/vuetify'
 import router from '@/plugins/router'
+import store from '@/store'
 import MyButton from '@/components/atoms/button.vue'
 
 Vue.config.productionTip = false
@@ -8,11 +9,13 @@ new Vue({
   el: '#app',
   vuetify,
   router,
+  store,
   components: { MyButton },
   template: `<v-app>
       <router-link to="/">to home</router-link>
       <router-link to="/about">to about</router-link>
       <router-view />
       <MyButton text="update" />
+      {{$store.state.test}}
    </v-app>`
 })
