7b43cc835e809b42b9a16ddc2e6bbff2d4a95f19
gobackk1 <k.kwbt4@icloud.com>
2022-12-12 08:59:44 +0900

    install firebase

---
+++ functions/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+    '.eslintrc.js'
+  ],
+  plugins: ['@typescript-eslint', 'import']
+  // rules は ../.eslintrc.json に合わせる
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,28 @@ import router from '@/plugins/router'
 import store from '@/store'
 import MyButton from '@/components/atoms/button.vue'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-_3PGHCl3qHT0Y-ogJIDXBezPXcfoPg0',
+  authDomain: 'chat-cord-4af6e.firebaseapp.com',
+  projectId: 'chat-cord-4af6e',
+  storageBucket: 'chat-cord-4af6e.appspot.com',
+  messagingSenderId: '811151546699',
+  appId: '1:811151546699:web:df7471edefcd2084a2b0ce',
+  measurementId: 'G-GB9CFS777E'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
+
 Vue.config.productionTip = false
 new Vue({
   el: '#app',
