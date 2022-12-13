a853df1f245b674fca8b145705c2689b061f6191
gobackk1 <k.kwbt4@icloud.com>
2022-12-13 09:12:21 +0900

    jest -  エラー潰し - ts-jest と babelの不整合を解決

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,7 +1,11 @@
 // jsdom-global は最初に宣言しないとエラーになる
 require('jsdom-global')()
 
+// https://github.com/vuejs/vue-test-utils/issues/974
+global.requestAnimationFrame = cb => cb()
+
 const Vue = require('vue')
 const Vuetify = require('vuetify')
 
+// TypeScriptを使う場合は localVue を使わない
 Vue.use(Vuetify)
