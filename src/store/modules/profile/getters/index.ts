0888854b3e970fef760fe4b6f50618d22d59da45
gobackk1 <k.kwbt4@icloud.com>
2022-12-17 12:41:25 +0900

    add uid getter

--- src/store/modules/profile/getters/index.ts
+++ src/store/modules/profile/getters/index.ts
@@ -1,3 +1,11 @@
-const getters = {}
+import { GetterTree } from 'vuex'
+import { ProfileState } from '../types'
+import { RootState } from '@/store/types'
+
+const getters: GetterTree<ProfileState, RootState> = {
+  uid({ loginUser }): string {
+    return loginUser ? loginUser.uid : ''
+  }
+}
 
 export default getters
