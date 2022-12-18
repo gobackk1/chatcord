00d56ad10e7c638f24377efbb310603fa90d55f0
gobackk1 <k.kwbt4@icloud.com>
2022-12-18 17:39:34 +0900

    友達リクエストのfunctions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,6 +26,45 @@ export const addPublicUserData = functions
       .set(publicUserData)
   })
 
+export const requestToBecomeFriend = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (request, response) => {
+    // TODO: 本番とローカル環境とでオリジンを変更する
+    response.set('Access-Control-Allow-Origin', 'http://localhost:8081')
+    response.set('Access-Control-Allow-Methods', 'POST')
+    response.set('Access-Control-Allow-Headers', 'Content-Type')
+
+    if (request.method === 'OPTIONS') {
+      response.send()
+    } else if (request.method === 'POST') {
+      const [target, requestUser] = await Promise.all([
+        admin.auth().getUser(request.body.sentTo),
+        admin.auth().getUser(request.body.sentFrom)
+      ])
+
+      const documentRef = await admin
+        .firestore()
+        .doc(`public_user_data/${target.uid}/friend_requests/${requestUser.uid}`)
+        .get()
+
+      if (documentRef.exists) {
+        response.send({ code: 'DOCUMENT_EXISTS' })
+      } else {
+        admin
+          .firestore()
+          .doc(`public_user_data/${target.uid}/friend_requests/${requestUser.uid}`)
+          .set({
+            requestUser: {
+              displayName: requestUser.displayName || '',
+              photoURL: requestUser.photoURL || '',
+              uid: requestUser.uid
+            }
+          })
+        response.send({ code: 'SUCESS' })
+      }
+    }
+  })
+
 // export default functions.firestore
 //   .document('/messages/{documentId}')
 //   .onCreate((snap, context) => {
