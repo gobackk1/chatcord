6b49db5a62366a8dde521bb00524e62b4280f9fc
gobackk1 <k.kwbt4@icloud.com>
2022-12-18 12:53:44 +0900

    functions でドキュメントを作成する

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,13 +6,30 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+// import addPublicUserData from './triggers/onCreate/addPublicUserData'
+import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
-export const makeUppercase = functions.firestore
-  .document('/messages/{documentId}')
-  .onCreate((snap, context) => {
-    const original = snap.data().original
-    console.log('Uppercasing', context.params.documentId, original)
-    const uppercase = original.toUpperCase()
-    return snap.ref.set({ uppercase }, { merge: true })
+admin.initializeApp()
+
+export const addPublicUserData = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate(user => {
+    const publicUserData = {
+      displayName: user.displayName!
+    }
+    return admin
+      .firestore()
+      .doc(`public_user_data/${user.uid}`)
+      .set(publicUserData)
   })
+
+// export default functions.firestore
+//   .document('/messages/{documentId}')
+//   .onCreate((snap, context) => {
+//     const original = snap.data().original
+//     console.log('Uppercasing', context.params.documentId, original)
+//     const uppercase = original.toUpperCase()
+//     return snap.ref.set({ uppercase }, { merge: true })
+//   })
--- src/components/App.vue
+++ src/components/App.vue
@@ -11,7 +11,6 @@ import Component from 'vue-class-component'
 import { namespace } from 'vuex-class'
 import { UserType } from '../store/modules/profile/types'
 import { onAuthStateChanged } from 'firebase/auth'
-import { firestoreAxios } from '@/plugins/axios'
 
 const Profile = namespace('profile')
 
@@ -25,24 +24,6 @@ export default class Cc_App extends Vue {
       console.log('subscribed user', user)
       if (user) {
         this.setLoginUser(user)
-
-        let response
-        try {
-          response = await firestoreAxios.get(`documents/public_user_data/${user.uid}`)
-          this.setPublicData(response.data)
-        } catch (error) {
-          if (error.response.data.error.status === 'NOT_FOUND') {
-            response = await firestoreAxios.post(
-              `documents/public_user_data?documentId=${user.uid}`,
-              {
-                init: 'init'
-              }
-            )
-            this.setPublicData(response.data)
-          } else {
-            throw new Error(error)
-          }
-        }
 
         if (user.emailVerified) {
           // Googleログインのリダイレクトから戻ってきた時、アプリ内へリダイレクトさせる
