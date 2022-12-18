7a970313b8a02c6d0523b30781eac797875baecd
gobackk1 <k.kwbt4@icloud.com>
2022-12-18 10:30:33 +0900

    公開可能なユーザーデータのドキュメントを作成する

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,18 +11,38 @@ import Component from 'vue-class-component'
 import { namespace } from 'vuex-class'
 import { UserType } from '../store/modules/profile/types'
 import { onAuthStateChanged } from 'firebase/auth'
+import { firestoreAxios } from '@/plugins/axios'
 
 const Profile = namespace('profile')
 
 @Component
 export default class Cc_App extends Vue {
   @Profile.Action('setLoginUser') setLoginUser!: (user: UserType) => void
+  @Profile.Action('setPublicData') setPublicData!: (user: UserType) => void
 
   created() {
-    onAuthStateChanged(Firebase.auth, user => {
+    onAuthStateChanged(Firebase.auth, async user => {
       console.log('subscribed user', user)
       if (user) {
         this.setLoginUser(user)
+
+        let response
+        try {
+          response = await firestoreAxios.get(`documents/public_user_data/${user.uid}`)
+          this.setPublicData(response.data)
+        } catch (error) {
+          if (error.response.data.error.status === 'NOT_FOUND') {
+            response = await firestoreAxios.post(
+              `documents/public_user_data?documentId=${user.uid}`,
+              {
+                init: 'init'
+              }
+            )
+            this.setPublicData(response.data)
+          } else {
+            throw new Error(error)
+          }
+        }
 
         if (user.emailVerified) {
           // Googleログインのリダイレクトから戻ってきた時、アプリ内へリダイレクトさせる
